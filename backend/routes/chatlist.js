3d1fa7487024ba8bd006ab25b17376880cab76e9
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-04-28 22:25:07 +0100

    Move code out of frontend folder

--- backend/routes/chatlist.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var { addChat, getChats } = require('../controllers/userchatlist');
-
-router.post('/add', addChat);
-router.post('/get', getChats);
-
-module.exports = router;
--- backend/routes/chats.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var { getChat, createChat, addMessage } = require('../controllers/chat');
-
-router.post('/findchat', getChat);
-router.post('/createchat', createChat);
-router.post('/addmessage', addMessage);
-
-module.exports = router;
