3f69143883ab1979753682843747f8c4e34edd02
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-05-04 22:04:51 +0100

    Remove commented code from navbar

--- src/components/lib/navbar.jsx
+++ src/components/lib/navbar.jsx
@@ -1,20 +1,18 @@
-import { Link } from 'react-router-dom';
-import capitalizeFirstLetter from './capitalise';
-import longLogo from '../homepage/images/longLogo.png';
-// import thumbTackLogo from './images/thumbTackLogo.png';
+import { Link } from "react-router-dom";
+import capitalizeFirstLetter from "./capitalise";
+import longLogo from "../homepage/images/longLogo.png";
 
 export default function NavBar({ linkTo }) {
   return (
-      <div className="navbar">
-         <div className="navbar-logo">
-          <img src={longLogo} alt="logo"/>
-        {/* <h1 className='logo'>Logo</h1> */}
+    <div className="navbar">
+      <div className="navbar-logo">
+        <img src={longLogo} alt="logo" />
       </div>
       <div className="navbar-links">
-        <Link to={`/${linkTo}`} className='link'>{capitalizeFirstLetter(linkTo)}</Link>
+        <Link to={`/${linkTo}`} className="link">
+          {capitalizeFirstLetter(linkTo)}
+        </Link>
       </div>
     </div>
   );
 }
-
-
