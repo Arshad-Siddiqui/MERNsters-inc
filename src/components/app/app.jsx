d0a83e1e238be1f3e358cf71fa2139d70edead24
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-05-04 12:45:51 +0100

    Amend import typo

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import Homepage from '../homepage/homepage';
-import Form from '../signup/Form';
-import { Route, Routes } from 'react-router-dom';
-import Footer from '../footer/footer';
-import Chat from '../chat/chat';
-import '../../App.css';
-import LoginPage from '../login/loginPage';
-import Swipe from '../swipe/swipe';
+import React from "react";
+import Homepage from "../homepage/homepage";
+import Form from "../signup/form";
+import { Route, Routes } from "react-router-dom";
+import Footer from "../footer/footer";
+import Chat from "../chat/chat";
+import "../../App.css";
+import LoginPage from "../login/loginPage";
+import Swipe from "../swipe/swipe";
 
 const App = () => {
   return (
@@ -17,7 +17,7 @@ const App = () => {
         <Route path="/signup" element={<Form />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path='/swipe' element={<Swipe />} />
+        <Route path="/swipe" element={<Swipe />} />
       </Routes>
       <Footer />
     </>
