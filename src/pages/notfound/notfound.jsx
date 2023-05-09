88f9dbfa767440ae309a9e25bd231e9dcfd065d6
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-05-09 12:02:01 +0100

    Add a return home to NotFound page

--- src/pages/notfound/notfound.jsx
+++ src/pages/notfound/notfound.jsx
@@ -5,6 +5,7 @@ export default function NotFound() {
     <div className="not-found-container">
       <h1>404: Page not found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
+      <a href="/">Return Home</a>
     </div>
   );
 }
