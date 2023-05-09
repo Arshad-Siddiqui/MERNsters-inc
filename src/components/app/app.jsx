7338bc66757cbb20173b912244d5f318d5dea56e
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-05-09 11:58:51 +0100

    Add NotFound page

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,16 +7,18 @@ import Chat from "../chat/chat";
 import "../../App.css";
 import LoginPage from "../login/loginPage";
 import Swipe from "../swipe/swipe";
+import NotFound from "../../pages/notfound/notfound";
 
 const App = () => {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Homepage />} />
         <Route path="/signup" element={<Form />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/swipe" element={<Swipe />} />
-        <Route path="/*" element={<Homepage />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
       <Footer />
     </>
