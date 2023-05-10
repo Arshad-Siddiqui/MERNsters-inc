93c033a4767a1a1d2f1adff2501d9157846a00e5
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-05-10 11:44:43 +0100

    Make footer a regular function

--- src/components/lib/footer/footer.jsx
+++ src/components/lib/footer/footer.jsx
@@ -1,22 +1,22 @@
 import "./footer.css";
 import React from "react";
 
-const Footer = () => (
-  <div className="footer">
-    <p>
-      <a href="www.facebook.com">
-        <i className="fa-brands fa-facebook fa-2x" />
-      </a>{" "}
-      &nbsp;
-      <a href="www.twitter.com">
-        <i className="fa-brands fa-twitter fa-2x" />
-      </a>{" "}
-      &nbsp;
-      <a href="www.instagram.com">
-        <i className="fa-brands fa-instagram fa-2x" />
-      </a>
-    </p>
-  </div>
-);
-
-export default Footer;
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>
+        <a href="www.facebook.com">
+          <i className="fa-brands fa-facebook fa-2x" />
+        </a>{" "}
+        &nbsp;
+        <a href="www.twitter.com">
+          <i className="fa-brands fa-twitter fa-2x" />
+        </a>{" "}
+        &nbsp;
+        <a href="www.instagram.com">
+          <i className="fa-brands fa-instagram fa-2x" />
+        </a>
+      </p>
+    </div>
+  );
+}
