51cc5b3edb8ebae97df440ed56b56a5bff0aa848
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-05-29 22:45:26 +0100

    Add max height to signup form

--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -46,42 +46,40 @@ export default function SignupForm() {
   };
 
   return (
-    <form onSubmit={submitForm} className="chonky-signup">
-      <div className="sign-up-container">
-        <input
-          type={"text"}
-          placeholder={"Username"}
-          value={formData.userName}
-          onChange={(event) =>
-            setFormData({ ...formData, userName: event.target.value })
-          }
-        />
+    <form className="sign-up-container" onSubmit={submitForm}>
+      <input
+        type={"text"}
+        placeholder={"Username"}
+        value={formData.userName}
+        onChange={(event) =>
+          setFormData({ ...formData, userName: event.target.value })
+        }
+      />
 
+      <input
+        type={"text"}
+        placeholder={"Email"}
+        value={formData.email}
+        onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+      />
+      <div>
         <input
-          type={"text"}
-          placeholder={"Email"}
-          value={formData.email}
-          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-        />
-        <div>
-          <input
-            type="password"
-            placeholder={"Password"}
-            value={formData.password}
-            onChange={(event) =>
-              setFormData({ ...formData, password: event.target.value })
-            }
-          />
-        </div>
-        <input
-          type={"password"}
-          placeholder={"Confirm Password"}
-          value={formData.confirmPassword}
+          type="password"
+          placeholder={"Password"}
+          value={formData.password}
           onChange={(event) =>
-            setFormData({ ...formData, confirmPassword: event.target.value })
+            setFormData({ ...formData, password: event.target.value })
           }
         />
       </div>
+      <input
+        type={"password"}
+        placeholder={"Confirm Password"}
+        value={formData.confirmPassword}
+        onChange={(event) =>
+          setFormData({ ...formData, confirmPassword: event.target.value })
+        }
+      />
     </form>
   );
 }
